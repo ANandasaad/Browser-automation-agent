33abfa8a654a0bbbe4859a04d6132226bcce4b8d
Anand kushwaha <anandkushwaha@Anands-MacBook-Air.local>
2026-07-17 01:31:49 +0530

    feature : added organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   return (
     <div className="relative min-h-svh p-6">
-      <div className="absolute right-6 top-6">
-        <UserButton  />
+      <div className="absolute right-6 top-6 flex items-center gap-4">
+        <OrganizationSwitcher />
+
+        <UserButton />
       </div>
     </div>
   )
